fd4bee42e0a2919bceb4579c491126dcf0a13c46
Jester Vergara <jester.vergara@gmail.com>
2024-11-06 01:39:28 +0800

    update control tower outputs.tf with correct syntax

--- aws/terraform/control-tower/main.tf
+++ aws/terraform/control-tower/main.tf
@@ -10,57 +10,57 @@ resource "aws_organizations_organization" "org" {
 
 # AWS Control Tower Accounts
 resource "aws_organizations_account" "shared_account" {
-  account_name           = "Shared"
+  name           = "Shared"
   email                  = var.shared_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 resource "aws_organizations_account" "log_archive_account" {
-  account_name           = "LogArchive"
+  name           = "LogArchive"
   email                  = var.log_archive_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 resource "aws_organizations_account" "audit_account" {
-  account_name           = "Audit"
+  name           = "Audit"
   email                  = var.audit_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 # Additional Accounts
 resource "aws_organizations_account" "prod_account" {
-  account_name           = "Prod"
+  name           = "Prod"
   email                  = var.prod_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 resource "aws_organizations_account" "dev_account" {
-  account_name           = "Dev"
+  name           = "Dev"
   email                  = var.dev_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 resource "aws_organizations_account" "qa_account" {
-  account_name           = "QA"
+  name           = "QA"
   email                  = var.qa_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 resource "aws_organizations_account" "sandbox_account" {
-  account_name           = "Sandbox"
+  name           = "Sandbox"
   email                  = var.sandbox_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 resource "aws_organizations_account" "logging_account" {
-  account_name           = "Logging"
+  name           = "Logging"
   email                  = var.logging_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
 resource "aws_organizations_account" "secops_account" {
-  account_name           = "SecOps"
+  name           = "SecOps"
   email                  = var.secops_email
-  organizational_unit_id = aws_organizations_organization.org.id
+  parent_id = aws_organizations_organization.org.id
 }
 
