ceaad8866acf8f49787e4d61758d61d616d533a8
Jester Vergara <jester.vergara@gmail.com>
2024-11-06 01:33:44 +0800

    update control tower outputs.tf with correct syntax

--- aws/terraform/control-tower/outputs.tf
+++ aws/terraform/control-tower/outputs.tf
@@ -4,37 +4,37 @@ output "organization_id" {
 }
 
 output "shared_account_id" {
-  value = aws_controltower_account.shared_account.id
+  value = aws_organizations_account.shared_account.id
 }
 
 output "log_archive_account_id" {
-  value = aws_controltower_account.log_archive_account.id
+  value = aws_organizations_account.log_archive_account.id
 }
 
 output "audit_account_id" {
-  value = aws_controltower_account.audit_account.id
+  value = aws_organizations_account.audit_account.id
 }
 
 output "prod_account_id" {
-  value = aws_controltower_account.prod_account.id
+  value = aws_organizations_account.prod_account.id
 }
 
 output "dev_account_id" {
-  value = aws_controltower_account.dev_account.id
+  value = aws_organizations_account.dev_account.id
 }
 
 output "qa_account_id" {
-  value = aws_controltower_account.qa_account.id
+  value = aws_organizations_account.qa_account.id
 }
 
 output "sandbox_account_id" {
-  value = aws_controltower_account.sandbox_account.id
+  value = aws_organizations_account.sandbox_account.id
 }
 
 output "logging_account_id" {
-  value = aws_controltower_account.logging_account.id
+  value = aws_organizations_account.logging_account.id
 }
 
 output "secops_account_id" {
-  value = aws_controltower_account.secops_account.id
+  value = aws_organizations_account.secops_account.id
 }
