c795ced4ca470214e7d975f7efa58043e0be3ba7
Jester Vergara <jester.vergara@gmail.com>
2024-11-06 02:31:34 +0800

    update control tower terraform to use modules

--- aws/terraform/control-tower/main.tf
+++ aws/terraform/control-tower/main.tf
@@ -1,3 +1,13 @@
+resource "aws_organizations_account" "this" {
+  name                      = var.account_name
+  email                     = var.account_email
+  parent_id                 = var.parent_id
+  role_name                 = var.role_name
+  iam_user_access_to_billing = var.iam_user_access_to_billing
+  close_on_deletion         = var.close_on_deletion
+  tags                      = var.tags
+}
+
 module "aws_organizations_account" {
   source = "../modules/account"
 
@@ -7,3 +17,4 @@ module "aws_organizations_account" {
   account_email = each.value
   parent_id     = aws_organizations_organization.org.roots[0].id
 }
+
--- aws/terraform/modules/account/main.tf
+++ aws/terraform/modules/account/main.tf
@@ -3,12 +3,4 @@ resource "aws_organizations_organization" "org" {
   feature_set                   = "ALL"
 }
 
-resource "aws_organizations_account" "this" {
-  name                      = var.account_name
-  email                     = var.account_email
-  parent_id                 = var.parent_id
-  role_name                 = var.role_name
-  iam_user_access_to_billing = var.iam_user_access_to_billing
-  close_on_deletion         = var.close_on_deletion
-  tags                      = var.tags
-}
+
