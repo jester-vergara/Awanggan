3bdaba9173ef6513cec42e725c86a4358336869a
Jester Vergara <jester.vergara@gmail.com>
2024-11-06 02:44:00 +0800

    update control tower terraform to use modules

--- aws/terraform/control-tower/outputs.tf
+++ aws/terraform/control-tower/outputs.tf
@@ -1,6 +1,6 @@
 output "account_ids" {
   value = {
-    for account, account_data in module.aws_organizations_account : account => account_data.id
+    for account, account_data in module.aws_organizations_account : account => account_data.account_id
   }
   description = "Account IDs for the AWS Control Tower accounts."
 }
@@ -9,3 +9,4 @@ output "organization_id" {
   value       = aws_organizations_organization.org.id
   description = "The ID of the AWS Organization."
 }
+
