3ac7eefef013c5d0c555aaf9b793806edc06e249
Jester Vergara <jester.vergara@gmail.com>
2024-11-07 22:41:46 +0800

    fix issue on workflow build step 1

--- aws/terraform/control-tower/dynamodb/main.tf
+++ aws/terraform/control-tower/dynamodb/main.tf
@@ -1,5 +1,5 @@
 module "dynamodb" {
-  source              = "../modules/dynamodb"
+  source              = "../../modules/dynamodb"
   dynamodb_table_name = var.dynamodb_table_name
   tags                = var.common_tags
 }
