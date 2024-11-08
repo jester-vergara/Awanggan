91a42ff30d7f75ee5b0e3b8b2723b56bf42603d5
Jester Vergara <jester.vergara@gmail.com>
2024-11-08 14:35:41 +0800

    fix underlying terraform jobs

--- aws/terraform/control-tower/s3/main.tf
+++ aws/terraform/control-tower/s3/main.tf
@@ -1,7 +1,7 @@
 module "s3" {
-  source = "./modules/s3"
+  source = "./../../modules/s3"
   s3_bucket_name = var.s3_bucket_name
   kms_key_id     = var.kms_key_id
-  iam_role_arn   = "arn:aws:iam::account-id:role/role-name" # Replace with actual role ARN
+  iam_role_arn   = module.iam.iam_role_arn
   tags           = var.common_tags
 }
