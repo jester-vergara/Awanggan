b9eecab42d9e27a5f1186839058f3323b6c1701c
Jester Vergara Laptop IKB15 <jester.vergara@proton.me>
2024-11-11 15:58:47 +0800

    fixing terraform import pipeline

--- aws/terraform/backend.tf
+++ aws/terraform/backend.tf
@@ -5,7 +5,5 @@ terraform {
     region               = var.aws_region
     dynamodb_table       = var.dynamodb_table
     encrypt              = var.encrypt_state
-    profile              = var.aws_profile
-    workspace_key_prefix = var.workspace_key_prefix
   }
 }
