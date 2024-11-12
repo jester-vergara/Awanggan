8adb7f25eb9ed7788dc24099e59861b4e2be369d
Jester Vergara <jester.vergara@gmail.com>
2024-11-12 22:37:32 +0800

    restructuring account resources for import

--- aws/terraform/dynamodb_resources.tf
+++ aws/terraform/dynamodb_resources.tf
@@ -1,5 +1,5 @@
 module "dynamodb" {
-  source              = "./../../modules/dynamodb"
+  source              = "./modules/dynamodb"
   dynamodb_table_name = local.dynamodb_table_name # Passing local as an input
   kms_key_id          = module.kms_key.kms_key_id # Pass the KMS key ID here
   depends_on          = [module.kms_key]
--- aws/terraform/s3_resources.tf
+++ aws/terraform/s3_resources.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source         = "./../../modules/s3"
+  source         = "./modules/s3"
   s3_bucket_name = var.s3_bucket_name
   kms_key_id     = var.kms_key_id
   iam_role_arn   = module.iam.iam_role_arn
