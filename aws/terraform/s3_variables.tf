9ed32a8b2727e4dd763388fc1ce3631e0b7191e3
Jester Vergara <jester.vergara@gmail.com>
2024-11-12 22:46:55 +0800

    restructuring account resources for import

--- aws/terraform/s3_variables.tf
+++ aws/terraform/s3_variables.tf
@@ -4,17 +4,8 @@ variable "s3_bucket_name" {
   default     = "awanggan-github-bootstrap-terraform-state-bucket"
 }
 
-variable "dynamodb_table_name" {
-  type        = string
-  description = "The name of the DynamoDB table for state locking"
-}
-
 variable "account_id" {
   type        = string
   description = "The AWS account ID"
 }
 
-variable "kms_key_id" {
-  type        = string
-  description = "The KMS key ID for encryption"
-}
