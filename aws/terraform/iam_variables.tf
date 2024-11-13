ff44d16d09ea4fb87ef349aa092dd72b51d22406
Jester Vergara Laptop IKB15 <jester.vergara@proton.me>
2024-11-13 16:46:24 +0800

    add s3 resource

---
+++ aws/terraform/iam_variables.tf
@@ -0,0 +1,4 @@
+variable "iam_role_arn" {
+  type        = string
+  description = "The ARN of the IAM role allowed access to the S3 bucket"
+}
--- aws/terraform/modules/s3/s3_module_resources.tf
+++ aws/terraform/modules/s3/s3_module_resources.tf
@@ -1,10 +1,6 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.s3_bucket_name
 
-  versioning {
-    enabled = true
-  }
-
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -17,18 +13,37 @@ resource "aws_s3_bucket" "terraform_state" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Bucket policy to restrict access to a specific IAM role
 resource "aws_s3_bucket_policy" "state_bucket_policy" {
   bucket = aws_s3_bucket.terraform_state.id
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "AllowPublicRead",
+        Sid       = "AllowSpecificIAMRoleAccess",
         Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.terraform_state.arn}/*"
+        Principal = {
+          AWS = var.iam_role_arn
+        },
+        Action    = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "${aws_s3_bucket.terraform_state.arn}",
+          "${aws_s3_bucket.terraform_state.arn}/*"
+        ]
       }
     ]
   })
 }
+
