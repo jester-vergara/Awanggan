e68a1cd12c61835195d29a69a5df22ede24cb705
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-18 18:57:31 +0800

    Create bootstrap resources

--- aws/terraform/modules/s3/s3_module_resources.tf
+++ aws/terraform/modules/s3/s3_module_resources.tf
@@ -1,10 +1,8 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.s3_bucket_name
-  }
 
-resource "aws_s3_bucket_acl" "terraform_state_acl" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private" # Replace "private" with the appropriate ACL if needed
+  # Apply tags directly here if your AWS provider version supports it
+  tags = var.tags
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
@@ -16,12 +14,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
       kms_master_key_id = var.kms_key_id
     }
   }
-}
-
-resource "aws_s3_bucket_tagging" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  tag_set = var.tags
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
