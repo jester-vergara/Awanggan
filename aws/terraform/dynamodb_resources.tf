02e9130391229be7e145c33362be63da3d8721b5
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-22 01:18:11 +0800

    Update kms module reference

--- aws/terraform/dynamodb_resources.tf
+++ aws/terraform/dynamodb_resources.tf
@@ -7,5 +7,5 @@ module "dynamodb_github" {
   read_capacity  = var.dynamodb_read_capacity
   write_capacity = var.dynamodb_write_capacity
   tags           = var.dynamodb_tags
-  kms_key_id     = var.dynamodb_kms_key_id
+  kms_key_id     = module.dynamodb_kms_key[0].kms_key_arn # Reference the output
 }
--- aws/terraform/s3_resources.tf
+++ aws/terraform/s3_resources.tf
@@ -2,6 +2,6 @@ module "s3_github" {
   source         = "./modules/s3"
   count          = terraform.workspace == "control_tower" ? 1 : 0
   s3_bucket_name = var.s3_bucket_name
-  kms_key_id     = var.s3_kms_key_id
+  kms_key_id     = module.s3_kms_key[0].kms_key_arn # Reference the output
   tags           = merge(var.common_tags, { CreationDate = "20241108" })
 }
