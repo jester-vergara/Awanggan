b0ccf5565f224e80bca2194328a5b96c9e6cbc34
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-23 20:32:39 +0800

    revert

--- aws/terraform/dynamodb_outputs.tf
+++ aws/terraform/dynamodb_outputs.tf
@@ -1,19 +1,19 @@
 output "dynamodb_table_name" {
   description = "The name of the DynamoDB table"
-  value       = module.dynamodb_github[0].table_name
+  value       = length(module.dynamodb_github) > 0 ? module.dynamodb_github[0].table_name : null
 }
 
 output "dynamodb_table_arn" {
   description = "The ARN of the DynamoDB table"
-  value       = module.dynamodb_github[0].table_arn
+  value       = length(module.dynamodb_github) > 0 ? module.dynamodb_github[0].table_arn : null
 }
 
 output "dynamodb_table_name_shared" {
   description = "The name of the DynamoDB table"
-  value       = module.dynamodb_github_shared[0].table_name
+  value       = length(module.dynamodb_github_shared) > 0 ? module.dynamodb_github_shared[0].table_name : null
 }
 
 output "dynamodb_table_arn_shared" {
   description = "The ARN of the DynamoDB table"
-  value       = module.dynamodb_github_shared[0].table_arn
+  value       = length(module.dynamodb_github_shared) > 0 ? module.dynamodb_github_shared[0].table_arn : null
 }
--- aws/terraform/kms_outputs.tf
+++ aws/terraform/kms_outputs.tf
@@ -1,19 +1,19 @@
 output "s3_kms_key_arn" {
-  value       = module.s3_kms_key[0].kms_key_arn
+  value       = length(module.s3_kms_key) > 0 ? module.s3_kms_key[0].kms_key_arn : null
   description = "The ARN of the KMS key for the S3 bucket"
 }
 
 output "dynamodb_kms_key_arn" {
-  value       = module.dynamodb_kms_key[0].kms_key_arn
+  value       = length(module.dynamodb_kms_key) > 0 ? module.dynamodb_kms_key[0].kms_key_arn : null
   description = "The ARN of the KMS key for the DynamoDB table"
 }
 
 output "s3_kms_key_arn_shared" {
-  value       = module.dynamodb_kms_key_shared[0].kms_key_arn
+  value       = length(module.dynamodb_kms_key_shared) > 0 ? module.dynamodb_kms_key_shared[0].kms_key_arn : null
   description = "The ARN of the KMS key for the S3 bucket"
 }
 
 output "dynamodb_kms_key_arn_shared" {
-  value       = module.dynamodb_kms_key_shared[0].kms_key_arn
+  value       = length(module.dynamodb_kms_key_shared) > 0 ? module.dynamodb_kms_key_shared[0].kms_key_arn : null
   description = "The ARN of the KMS key for the DynamoDB table"
 }
