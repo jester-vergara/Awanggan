7950b8ef32bed905cf24212bf9c05b9acdd49db4
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-23 16:40:45 +0800

    Resolve shared pipeline

--- aws/terraform/s3_outputs.tf
+++ aws/terraform/s3_outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_arn" {
   description = "ARN of the S3 bucket used for Terraform state"
-  value       = module.s3_github[0].s3_bucket_arn
+  value       = length(module.s3_github) > 0 ? module.s3_github[0].s3_bucket_arn : null
 }
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket used for Terraform state"
-  value       = module.s3_github[0].s3_bucket_name
+  value       = length(module.s3_github) > 0 ? module.s3_github[0].s3_bucket_name : null
 }
