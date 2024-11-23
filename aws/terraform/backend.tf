3c8fd0ef85bba1f6ce0e8c9f8a3c29da40d4bfb9
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-23 22:29:39 +0800

    retry local tf apply and upload statefile

--- aws/terraform/backend.tf
+++ aws/terraform/backend.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket         = var.s3_bucket_name
-    key            = "terraform/state/${var.environment}/${var.environment}.tfstate"
-    region         = var.aws_region
-    dynamodb_table = var.dynamodb_table_name
-    encrypt        = var.encrypt_state
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = var.s3_bucket_name
+#     key            = "terraform/state/${var.environment}/${var.environment}.tfstate"
+#     region         = var.aws_region
+#     dynamodb_table = var.dynamodb_table_name
+#     encrypt        = var.encrypt_state
+#   }
+# }
 # terraform {
 #   backend "local" {
 #     path = "./terraform.tfstate"
---
+++ aws/terraform/control_tower.backend.hcl
@@ -0,0 +1,5 @@
+bucket         = "your-s3-bucket-name"
+key            = "terraform/state/control_tower/control_tower.tfstate"
+region         = "your-region"
+dynamodb_table = "your-dynamodb-table-name"
+encrypt        = true
