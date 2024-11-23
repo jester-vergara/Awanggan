b58cad3201fd3b3c5a8cda0a7e2d9d59a272a940
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-23 15:13:48 +0800

    Resolve shared pipeline

--- aws/terraform/backend.tf
+++ aws/terraform/backend.tf
@@ -1,14 +1,14 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = var.s3_bucket_name
-#     key            = "terraform/state/${var.environment}/${var.environment}.tfstate"
-#     region         = var.aws_region
-#     dynamodb_table = var.dynamodb_table_name
-#     encrypt        = var.encrypt_state
-#   }
-# }
 terraform {
-  backend "local" {
-    path = "./terraform.tfstate"
+  backend "s3" {
+    bucket         = var.s3_bucket_name
+    key            = "terraform/state/${var.environment}/${var.environment}.tfstate"
+    region         = var.aws_region
+    dynamodb_table = var.dynamodb_table_name
+    encrypt        = var.encrypt_state
   }
 }
+# terraform {
+#   backend "local" {
+#     path = "./terraform.tfstate"
+#   }
+# }
