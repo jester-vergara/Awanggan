12d7f5ea57881bdb34ebe6eaa3569d0c4a5f41d2
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-23 15:12:55 +0800

    Resolve shared pipeline

--- aws/terraform/dynamodb_outputs.tf
+++ aws/terraform/dynamodb_outputs.tf
@@ -1,9 +1,9 @@
 output "dynamodb_table_name" {
   description = "The name of the DynamoDB table"
-  value       = module.dynamodb_github[0].table_name
+  value       = length(module.dynamodb_github) > 0 ? module.dynamodb_github[0].table_name : null
 }
 
 output "dynamodb_table_arn" {
   description = "The ARN of the DynamoDB table"
-  value       = module.dynamodb_github[0].table_arn
+  value       = length(module.dynamodb_github) > 0 ? module.dynamodb_github[0].table_arn : null
 }
--- aws/terraform/shared.tfvars
+++ aws/terraform/shared.tfvars
@@ -2,3 +2,11 @@ environment = "shared"
 
 dynamodb_table_name = "gitlab-terraform-state-lock"
 s3_bucket_name      = "awanggan-gitlab-terraform-state-bucket"
+
+kms_key_admins = [
+  "arn:aws:iam::961341517694:role/GitHubAssumeRoleAwanggan"
+]
+
+kms_key_users = [
+  "arn:aws:iam::961341517694:role/GitHubAssumeRoleAwanggan"
+]
