d7ad400a4bc20ea7a5743d028ab71d2301195ed7
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-25 17:30:58 +0800

    Update Terraform version and provider config

--- aws/terraform/providers.tf
+++ aws/terraform/providers.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
-}
-
-variable "aws_region" {
-  default     = "ap-southeast-1"
-  description = "AWS region to deploy resources"
 }
--- aws/terraform/versions.tf
+++ aws/terraform/versions.tf
@@ -1,9 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0" # Choose a stable version
+      version = "~> 5.0"
     }
   }
 }
