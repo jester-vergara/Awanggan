1e583d020a2f8fc2721b16c8bb5fd43c398faf12
jesterramon.vergara <jesterramon.vergara@proton.me>
2024-11-25 17:25:46 +0800

    Add policy definition to DynamoDB

--- aws/terraform/providers.tf
+++ aws/terraform/providers.tf
@@ -1,3 +1,10 @@
-provider "aws" {
-  region = "ap-southeast-1"
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Use latest compatible version in 5.x
+    }
+  }
 }
