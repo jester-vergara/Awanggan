8778cb640f725d19c132636e715d487d4bd63e29
jesterramon.vergara <jesterramon.vergara@proton.me>
2025-02-24 16:59:09 +0800

    Test subnet and vpc module interdependency

--- aws/terraform/subnet_resources.tf
+++ aws/terraform/subnet_resources.tf
@@ -3,11 +3,11 @@ module "subnet_shared" {
   source = "./modules/subnet"
 
   for_each = terraform.workspace == "shared" ? {
-    for vpc_key, vpc in module.vpc_shared : vpc_key => vpc
+    for vpc_key, vpc in local.vpcs_to_create : vpc_key => vpc
   } : {}
 
-  vpc_id  = each.value.vpc_id
-  name    = each.value.name
+  vpc_id  = module.vpc_shared[each.key].vpc_id
+  name    = module.vpc_shared[each.key].name
   subnets = var.subnet_cidr[each.value.account][each.value.region][each.value.label]
-  tags    = each.value.tags
+  tags    = module.vpc_shared[each.key].tags
 }
--- aws/terraform/vpc_resources.tf
+++ aws/terraform/vpc_resources.tf
@@ -26,7 +26,20 @@
 module "vpc_shared" {
   source = "./modules/vpc"
 
-  for_each = local.vpcs_to_create
+  for_each = terraform.workspace == "shared" ? {
+    for vpc in var.vpc_to_create :
+    join("-", vpc) => {
+      name       = "VPC-${vpc[0]}-${vpc[1]}-${vpc[2]}"
+      cidr_block = var.vpc_cidr[vpc[0]][vpc[1]][vpc[2]]
+      account    = vpc[0]
+      region     = vpc[1]
+      label      = vpc[2]
+      tags = {
+        Project     = "Awanggan"
+        Environment = terraform.workspace
+      }
+    }
+  } : {}
 
   name       = each.value.name
   cidr_block = each.value.cidr_block
